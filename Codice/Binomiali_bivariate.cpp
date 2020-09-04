d1c522c6ab14b4dba5b47e5a94b36549b12f6be7
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2020-09-04 18:53:07 +0200

    Quasi finito l'esercizio sulle leggi normali.

--- Codice/Binomiali_bivariate.cpp
+++ Codice/Binomiali_bivariate.cpp
@@ -9,11 +9,33 @@
 #include "rapidcsv.h"
 
 using namespace std;
+//Utilizzo della miglior libreria per estrazione di dati csv
+//Carico la tabella
+rapidcsv::Document doc("tavola_normale.csv", rapidcsv::LabelParams(0, 0));
+
+/*
+Estrae i valori dalle tavole della funzione di ripartizione della legge normale standard
+(che nome lungo per nulla !!)
+*/
+double trova(double zd) {
+	string z{};
+	z = to_string(zd);
+	string yaxis = z.substr(0, 3);
+	string xaxis = "0.0"+z.substr(3, 1);
+
+	cout << "\nEstrazione delle tavole:\n";
+	cout << endl << "y= " << yaxis;
+	cout << endl << "x= " << xaxis;
+	cout << endl;
+	cout << "//////////////////////\n";
+	
+	//Straggo dal csv
+	double estrazioneTavola = doc.GetCell<double>(xaxis, yaxis);
+	
+	return estrazioneTavola;
+}
 
 int main() {
-	//Utilizzo della miglior libreria per estrazione di dati csv
-	//Carico la tabella
-	rapidcsv::Document doc("tavola_normale.csv", rapidcsv::LabelParams(0, 0));
 	
 	//Inserimento dei dati
 	double u = 0, a2 = 0, n = 0, A = 0, B = 0;
@@ -39,24 +61,25 @@ int main() {
 	cout << "MYn(t) = " << "exp(" << u << "t+0.5t^2 * " << a2 << "/n)"<<endl;
 
 	//Parcellizzo il risultato
-
-
+	//Estrazione del valore dalle tavole
+	double t = A, sigma = 0;
+	double dacercare = abs((t - u)/a);
+	if (t - u < 0)	sigma = 1 - trova(dacercare);
+	else			sigma = trova(dacercare);
 	
-	//double estrazioneTavola = doc.GetCell<double>( "0.01", "0.1");
+	double PYnA = 1 - sigma;
+	cout << "P(Y" << n << " > " << A << ")= "; cout << PYnA<<endl;
 
-	//cout << estrazioneTavola;
 
+	//Parcellizzo il risultato
+	//Estrazione del valore dalle tavole
+	t = B; sigma = 0;
+	dacercare = abs((t - u) / a);
+	if (t - u < 0)	sigma = 1 - trova(dacercare);
+	else			sigma = trova(dacercare);
 
-	/*
-	string derivataprima = "xdg-open https://www.wolframalpha.com/input/?i=D(D(e^(2*(4e^(t)-4))))";
-	cout << derivataprima << endl;
-	string d{};
-	for (auto c : derivataprima) {
-		if (c == '(') d += "%28";
-		else if (c == ')') d += "%29";
-		else d += c;
-	}
-	system(d.c_str());
-	*/
+	double PYnB = sigma;
+	cout << "P(Y" << n << " < " << B << ")= "; cout << PYnB << endl;
+	
 	return 0;
 }
